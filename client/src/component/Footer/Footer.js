578571a2a5db1fe770d51fc1ba1683a46970b515
Eshwar Sharma T <105738487+killshot144@users.noreply.github.com>
2025-07-01 00:31:38 +0530

    Add files via upload

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+                            </p>
+        <p>
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
